a76407d27f58ca027a4091ff47dad971929a6544
sowmiya <31585889+sowmiyasuba@users.noreply.github.com>
2022-08-15 17:08:54 +0530

    Create ThreadGroupDemo.java

---
+++ multithreading/ThreadGroupDemo.java
@@ -0,0 +1,14 @@
+public class ThreadGroupDemo {
+
+    public static void main(String[] args)
+    {
+        ThreadGroup g=new ThreadGroup("First Group");
+        System.out.println(Thread.currentThread().getThreadGroup().getParent().getName());
+        System.out.println(g.getParent().getName());
+
+        ThreadGroup g1=new ThreadGroup(g,"Second Group");
+        System.out.println(g1.getParent().getName());
+
+        //system---main--first group--second group
+    }
+}
